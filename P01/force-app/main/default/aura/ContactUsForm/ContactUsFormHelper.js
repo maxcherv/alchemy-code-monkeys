47d5fd87a6f8b57bad45abc6b6c91f2d08046924
jacobmjohnson1999 <acureisinsight@gmail.com>
2021-09-15 14:44:45 -0500

    Added some functionality for Contact us

--- P01/force-app/main/default/aura/ContactUsForm/ContactUsFormHelper.js
+++ P01/force-app/main/default/aura/ContactUsForm/ContactUsFormHelper.js
@@ -22,6 +22,19 @@
 
         let sendEmail = component.get("c.emailMaker");
 
+        //Gets values from the component
+        let fName = component.get("v.firstName");
+        let lName = component.get("v.lastName");
+        let email = component.get("v.email");        
+        let mySubject = component.get("v.subject");
+        let message = component.get("v.message");
+
+        //Temporary email to send to
+        let toAddress = "acureisinsight@gmail.com";
+
+        //Sets up the body
+        let myBody = fName + " " + lName + " has submitted a question. The return email is: " + email + " with a body of:\n" + message;
+
         sendEmail.setParams({
 
             "mySubject" : mySubject,
@@ -30,5 +43,20 @@
 
         })
 
+        sendEmail.setCallback(this, function(response){
+
+            let myStatus = response.getStatus();
+
+            if(myStatus === "SUCCESS"){
+
+                console.log("Callout successful");
+
+            }else{
+                console.log("There was an error calling out to the apex class");
+            }
+
+        })
+
+        $A.enqueueAction(sendEmail);
     }
 })
