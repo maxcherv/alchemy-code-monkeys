4060cdee95d8fdd85bcb0f8df1ff1d1be41055f7
HellPwnage1337 <hellpwnage1337@gmail.com>
2021-09-17 16:32:20 -0500

    Testing Finished

--- P01/force-app/main/default/aura/ProductSearch/ProductSearchController.js
+++ P01/force-app/main/default/aura/ProductSearch/ProductSearchController.js
@@ -1,23 +1,24 @@
-({
+(
+    {
+    search: function(component, event, helper) 
+    {
+		var name=component.get("v.SearchName");
+        var type = component.get("v.SearchType");
+        var style=component.get("v.SearchStyle");
+        var products = helper.GetProducts(component, event, name, type, style);
+       
+    },
     DoInit: function(component, event, helper)
     {
         component.set("v.Fields", 
                       [
                           	{label: 'Product Name', fieldName: 'Name', type: 'text'},
                       		{label: 'Carat', fieldName: 'Carat__c', type: 'number'},
-                            {label: 'Product Type', fieldName: 'Product_Type__c', type: 'text'},
-                            {label: 'Product Style', fieldName: 'Product_Style__c', type: 'text'},
-                            
-                      {label: 'Price', fieldName: 'Retail_Price1__c', type: 'currency'}
+                            {label: 'Product Type', fieldName: 'Product_Type__c', type: 'string'},
+                            {label: 'Product Style', fieldName: 'Product_Style__c', type: 'string'},
+                          {label: 'Price', fieldName: 'Retail_Price1__c', type: 'currency', typeAttributes: {currencyCode: 'USD'}}
                             
                       ]);
-    }
-},{
-	searchProducts : function(component, event, helper) {
-		var name=component.get("v.SearchName");
-        var type = component.get("v.SearchType");
-        var style=component.get("v.SearchStyle");
-        var products = helper.GetProducts(component, event, name, type, style);
-       
-	}
-})
+
+    },
+    });
--- P01/force-app/main/default/aura/ProductSearch/ProductSearchHelper.js
+++ P01/force-app/main/default/aura/ProductSearch/ProductSearchHelper.js
@@ -7,16 +7,20 @@
              "style" : style});
         action.setCallback(this, function(response)
             {
+                console.log(response.getState());
                 if(response.getState()==="SUCCESS")
                 {
+                    console.log("Data Retrieved");
             		component.set("v.Products", response.getReturnValue());
+                    console.log(component.get("v.Products"));
         		}	
                 else if (response.getState() === "ERROR")
                 {
-                
+                    component.set("v.Products", '');
+                	console.log("Data Not Retrieved");
                 }
                 else{
-                  
+                  console.log("Data Not Retrieved");
                 }
         });
         $A.enqueueAction(action);
